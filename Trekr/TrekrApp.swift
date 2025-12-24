c3e34759b00e6f04895510884337b9ed7ac525e4
Vansh Nagpal <vanshnagpal21@gmail.com>
2025-12-24 20:16:27 +0530

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Vansh Nagpal on 24/12/25.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
